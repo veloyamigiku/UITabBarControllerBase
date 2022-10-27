240de57d869499555748b7fb7ea1757b6470ed67
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2022-10-27 22:25:32 +0900

    Initial Commit

---
+++ UITabBarControllerBase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITabBarControllerBase
+//
+//  Created by velo.yamigiku@gmail.com on 2022/10/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITabBarControllerBase/UITabBarControllerBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITabBarControllerBaseApp.swift
+//  UITabBarControllerBase
+//
+//  Created by velo.yamigiku@gmail.com on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct UITabBarControllerBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
